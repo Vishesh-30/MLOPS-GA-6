4e30b66a0737e7a3a97aa5d755f9c7d1ab439511
Vishesh-30 <visheshphutela30@gmail.com>
2025-07-26 16:46:09 +0000

    Initial commit: CML GitHub Actions CD pipeline

---
+++ app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+app = FastAPI()
+model = joblib.load("iris-classifier-week-1_model.joblib")
+
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+@app.get("/")
+def home():
+    return {"message": "Iris Predictor API running!"}
+
+@app.post("/predict")
+def predict(data: IrisInput):
+    input_df = pd.DataFrame([data.dict()])
+    prediction = model.predict(input_df)[0]
+    return {"predicted_class": prediction}
+
